1c1ad29a7e18f6e3beff0a5fedb55a8cb7f54583
Johan george <117387655+ft-johan@users.noreply.github.com>
2024-03-10 21:42:34 +0530

    Add files via upload

---
+++ app/(home)/components/Skills.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from 'react'
+import Title from './Title'
+import { SiAbletonlive, SiCss3, SiFirebase, SiGit, SiHtml5, SiJavascript, SiKotlin, SiNextdotjs, SiReact, SiTailwindcss } from 'react-icons/si'
+import { HoverEffect } from '@/components/ui/card-hover-effect'
+
+export default function Skills() {
+    const skills = [
+        {
+            text: "React",
+            Icon: SiReact,
+        },
+        {
+            text: "Kotlin",
+            Icon: SiKotlin,
+        },
+        {
+            text: "Next.js",
+            Icon: SiNextdotjs,
+        },
+        {
+            text: "Tailwind",
+            Icon: SiTailwindcss,
+        },
+        {
+            text: "Firebase",
+            Icon: SiFirebase,
+        },
+        {
+            text: "Javascript",
+            Icon: SiJavascript,
+        },
+        {
+            text: "HTML5",
+            Icon: SiHtml5,
+        },
+        {
+            text: "CSS3",
+            Icon: SiCss3,
+        },
+        {
+            text: "Git",
+            Icon: SiGit,
+        },
+
+    ]
+  return (
+
+    <div className="max-w-5xl mx-auto px-8">
+        <Title text='Skills' className="flex flex-col items-center justify-center rotate-6"/>
+        <HoverEffect items={skills} /> 
+    </div>
+  )
+}
